b449e52ac397363c685707c87bfb8f31f904884c
Binay Dahal <bidahal@deerwalk.com>
2016-10-24 15:55:33 +0545

    minor changes

--- src/main/java/controllers/home.java
+++ src/main/java/controllers/home.java
@@ -7,6 +7,7 @@ public class home {
     public void index() {
 //        System.out.println("from controller:::::" + params.Long("id"));
         System.out.println("Params from controller::::: " + params.getParams("test"));
+        System.out.println("Params2::::: "+params.getParams("test2"));
     }
 
     public void show(String text) {
